545caf2a1f6cba90b28cd03f48cec323fe9f53b6
Cobbie Andrei de los Reyes <cobbiedelosreyes17@gmail.com>
2022-05-12 13:36:29 +0800

    Created API

--- MyAPI/models.py
+++ MyAPI/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class vehicles(models.Model):
     name = models.CharField(max_length=60)
-    description = models.Charfield(max_length=100)
+    description = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
---
+++ MyAPI/serializers.py
@@ -0,0 +1,9 @@
+
+from rest_framework import serializers
+from .models import vehicles
+
+class vehiclesSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = vehicles
+        fields = ('name', 'description')
+        
